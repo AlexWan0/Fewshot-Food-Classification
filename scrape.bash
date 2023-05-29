adff067bc7b64e795ebd554d52a6790b68a68402
Alexander Wan <wanalex03@gmail.com>
2023-05-29 02:34:27 +0000

    bash script changes

--- scrape.bash
+++ scrape.bash
@@ -1,11 +1,19 @@
+#!/bin/bash
+
+name='test'
+
+mkdir dataset/$name
+mkdir dataset/$name/images
+
 python scrape_articles.py \
     --category "Category:Noodles" \
-    --out_file dataset/test/article_text.json \
+    --out_file dataset/$name/article_text.json \
     --max_depth 0
 
 python scrape_images.py \
-    --article_file dataset/test/article_text.json \
-    --dataset_dir dataset/test/dataset \
-    --url_mapping dataset/test/url_mapping.json \
-    --label_mapping dataset/test/label_mapping.json \
-    --image_dir dataset/test/images
+    --article_file dataset/$name/article_text.json \
+    --dataset_dir dataset/$name/dataset \
+    --url_mapping dataset/$name/url_mapping.json \
+    --label_mapping dataset/$name/label_mapping.json \
+    --image_dir dataset/$name/images
+
